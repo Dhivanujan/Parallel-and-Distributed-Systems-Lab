d3ec4a3bc5d1bf732f199e811eaebd126f6899f1
Dhivanujan <mahadevvinayak837@gmail.com>
2026-04-23 11:31:03 +0530

    Add SyncCounter2 class with multithreading example using shared counter

---
+++ Lab5/SyncCounter2.java
@@ -0,0 +1,34 @@
+class Counter {
+    private int count = 0;
+
+    public void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+public class SyncCounter2 {
+    public static void main(String[] args) throws InterruptedException {
+        Counter sharedCounter = new Counter();
+
+        Runnable task = () -> {
+            for (int i = 0; i < 10000; i++) {
+                sharedCounter.increment();
+            }
+        };
+
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Final Count: " + sharedCounter.getCount());
+    }
+}
